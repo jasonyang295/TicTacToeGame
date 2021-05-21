87d58a0804fcc4ff014d437ca5118931f0b91152
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 20:52:22 -0700

    initialized gameboard

---
+++ tictactoe.java
@@ -0,0 +1,17 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] gameboard = {{' ', '|', ' ', '|', ' '}, 
+        {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}, 
+        {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}};
+        printgameboard(gameboard);
+    }
+    public static void printgameboard(char[][] gameboard) {
+        
+        for(char[] row : gameboard) {
+            for(char c: row) {
+                System.out.println(c);
+            }
+            System.out.println();
+        }
+    }
+}
