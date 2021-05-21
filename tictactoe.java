0d360fe070f5c9d89cc976882da4462a0ba30cdb
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 21:14:50 -0700

    made scanner object

--- tictactoe.java
+++ tictactoe.java
@@ -1,15 +1,25 @@
+import java.util.*;
+
 class TicTacToe {
     public static void main(String[] args) {
         char[][] gameboard = {{' ', '|', ' ', '|', ' '}, 
         {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}, 
         {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}};
         printgameboard(gameboard);
+
+        Scanner scaninput = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9):");
+        int pos = scaninput.nextInt();
+        System.out.println(pos);
+
+
+        //scaninput.close();
     }
     public static void printgameboard(char[][] gameboard) {
 
         for(char[] row : gameboard) {
             for(char c: row) {
-                System.out.println(c);
+                System.out.print(c);
             }
             System.out.println();
         }
