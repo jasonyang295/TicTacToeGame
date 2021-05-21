a5602e533f0ff6f2c230f63b3f21080c776d5f2b
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 21:08:33 -0700

    small comment

--- tictactoe.java
+++ tictactoe.java
@@ -1,4 +1,4 @@
-public class TicTacToe {
+class TicTacToe {
     public static void main(String[] args) {
         char[][] gameboard = {{' ', '|', ' ', '|', ' '}, 
         {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}, 
@@ -6,12 +6,13 @@ public class TicTacToe {
         printgameboard(gameboard);
     }
     public static void printgameboard(char[][] gameboard) {
-        
+
         for(char[] row : gameboard) {
             for(char c: row) {
                 System.out.println(c);
             }
             System.out.println();
         }
+        //comment to make sure stuff is working
     }
 }
