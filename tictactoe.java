9044beecfcc2638cc4c01f05bd021826dfea9052
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 21:26:46 -0700

    big ass update
    created loop conditions, and can play the game now. doesn't know win condition but will keep looping and fill in pieces

--- tictactoe.java
+++ tictactoe.java
@@ -7,12 +7,25 @@ class TicTacToe {
         {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}};
         printgameboard(gameboard);
 
-        Scanner scaninput = new Scanner(System.in);
-        System.out.println("Enter your placement (1-9):");
-        int pos = scaninput.nextInt();
-        System.out.println(pos);
+        
+
+        while (true) {
+            Scanner scaninput = new Scanner(System.in);
+            System.out.println("Enter your placement (1-9):");
+            int playerpos = scaninput.nextInt();
+            System.out.println(playerpos);
+        
+            placePiece(gameboard, "player", playerpos);
+        
+
+            Random rand = new Random();
+            int cpuPosition = rand.nextInt(9) + 1; //establishes almost AI feeling
+            placePiece(gameboard, "cpu", cpuPosition);
 
 
+            printgameboard(gameboard);
+        }
+        
         //scaninput.close();
     }
     public static void printgameboard(char[][] gameboard) {
@@ -24,5 +37,47 @@ class TicTacToe {
             System.out.println();
         }
         //comment to make sure stuff is working
+    }
+    public static void placePiece(char[][] gameboard, String user, int pos) {
+        
+        char symbol = 'X';
+
+        if (user.equals("player")) {
+            symbol = 'X';
+        } else {
+            symbol = '0';
+        }
+        
+        switch(pos) {
+            case 1: 
+                gameboard[0][0] = symbol ;
+                break;
+            case 2: 
+                gameboard[0][2] = symbol ;
+                break;
+            case 3: 
+                gameboard[0][4] = symbol ;
+                break;
+            case 4: 
+                gameboard[2][0] = symbol ;
+                break;
+            case 5: 
+                gameboard[2][2] = symbol ;
+                break;
+            case 6: 
+                gameboard[2][4] = symbol ;
+                break;
+            case 7: 
+                gameboard[4][0] = symbol ;
+                break;
+            case 8: 
+                gameboard[4][2] = symbol ;
+                break;
+            case 9: 
+                gameboard[4][4] = symbol ;
+                break;
+            default:
+                break;
+        }
     }
 }
